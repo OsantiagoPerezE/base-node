bd3d5875eb9c8ede89ef5b30704f4f69105c2613
ospereze-CAIO <ospereze@grupo-caio.com>
2023-08-27 20:11:22 -0500

    first commit

---
+++ clients/users.js
@@ -0,0 +1,24 @@
+const {Router} = require('express');
+
+const {
+	usersGet,
+	usersPut,
+	usersPost,
+	usersDelete,
+	usersPatch,
+	loginPost,
+} = require('../controllers/users');
+
+const router = Router();
+
+router.get('/', usersGet);
+
+router.put('/:id', usersPut);
+
+router.post('/', usersPost);
+
+router.delete('/', usersDelete);
+
+router.patch('/', usersPatch);
+
+module.exports = router;
